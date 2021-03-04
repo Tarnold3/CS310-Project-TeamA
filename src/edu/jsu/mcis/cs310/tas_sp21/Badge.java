eed1c9cdc178cf9d5d78d1589fba388fbd3582f8
Edward <eweldon@stu.jsu.edu>
2021-03-04 14:52:31 -0600

    Badge Class Complete

--- src/edu/jsu/mcis/cs310/tas_sp21/Badge.java
+++ src/edu/jsu/mcis/cs310/tas_sp21/Badge.java
@@ -11,4 +11,34 @@ package edu.jsu.mcis.cs310.tas_sp21;
  */
 public class Badge {
     
+    private String id;
+    private String description;
+
+    public Badge(String id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        //return "Badge{" + "id=" + id + ", description=" + description + '}';
+        StringBuilder s = new StringBuilder();
+        
+        s.append("#").append(id).append(" ");
+        s.append("(").append(description).append(")");
+        
+        return (s.toString());
+    }
+
+    
+    
+    
 }
